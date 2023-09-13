a0373cab87921fa55652ec5eb9c042825f52f5da
August Femtehjell <augustfe@math.uio.no>
2023-09-13 20:03:33 +0200

    reformatted using black

--- Project1/DataFiles/test.py
+++ Project1/DataFiles/test.py
@@ -7,24 +7,24 @@ import numpy as np
 from numpy.random import normal, uniform
 
 # Load the terrain
-terrain = imread('SRTM_data_Norway_1.tif')
+terrain = imread("SRTM_data_Norway_1.tif")
 
 N = 1000
-m = 5 # polynomial order
-terrain = terrain[:N,:N]
+m = 5  # polynomial order
+terrain = terrain[:N, :N]
 # Creates mesh of image pixels
-x = np.linspace(0,1, np.shape(terrain)[0])
-y = np.linspace(0,1, np.shape(terrain)[1])
-x_mesh, y_mesh = np.meshgrid(x,y)
+x = np.linspace(0, 1, np.shape(terrain)[0])
+y = np.linspace(0, 1, np.shape(terrain)[1])
+x_mesh, y_mesh = np.meshgrid(x, y)
 
 z = terrain
-X = create_X(x_mesh, y_mesh,m)
+X = create_X(x_mesh, y_mesh, m)
 
 
 # Show the terrain
 plt.figure()
-plt.title('Terrain over Norway 1')
-plt.imshow(terrain, cmap='gray')
-plt.xlabel('X')
-plt.ylabel('Y')
+plt.title("Terrain over Norway 1")
+plt.imshow(terrain, cmap="gray")
+plt.xlabel("X")
+plt.ylabel("Y")
 plt.show()
