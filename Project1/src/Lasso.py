11e1d558b41ac77ff7a084569f2a7ca0a81e8a70
August Femtehjell <augustfe@math.uio.no>
2023-09-20 18:33:52 +0200

    Added Part C

--- Project1/src/Lasso.py
+++ Project1/src/Lasso.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.linear_model import Lasso
 from sklearn.preprocessing import StandardScaler
-from util import MSE
+from util import MSE, R2Score
 
 
 def pureSKLearnLasso(
@@ -10,7 +10,8 @@ def pureSKLearnLasso(
     y_train: np.array,
     y_test: np.array,
     alpha: float = 0.1,
-) -> tuple[float, float]:
+    scale: bool = False,
+) -> tuple[float, float, float]:
     """Using scikit to solve with Lasso.
 
     inputs:
@@ -19,23 +20,26 @@ def pureSKLearnLasso(
         y_train: Training values
         y_test: Testing data
         alpha (float): Regularization strength
+        scale (bool): whether to scale data in func
     returns:
         Mean squared error of trained and predicted.
     """
     # Scale the data
-    scaler = StandardScaler().fit(X_train)
+    if scale:
+        scaler = StandardScaler().fit(X_train)
 
-    scaled_X_train = scaler.transform(X_train)
-    scaled_X_test = scaler.transform(X_test)
+        X_train = scaler.transform(X_train)
+        X_test = scaler.transform(X_test)
 
     # Initialize and fit model
     clf = Lasso(alpha=alpha)
-    clf.fit(scaled_X_train, y_train)
+    clf.fit(X_train, y_train)
 
-    ztilde = clf.predict(scaled_X_train)
-    z_pred_Lasso = clf.predict(scaled_X_test)
+    ztilde = clf.predict(X_train)
+    z_pred_Lasso = clf.predict(X_test)
 
     MSE_train = MSE(y_train, ztilde)
     MSE_test = MSE(y_test, z_pred_Lasso)
+    R2 = R2Score(y_test, z_pred_Lasso)
 
-    return MSE_train, MSE_test
+    return MSE_train, MSE_test, R2
--- Project1/src/PartC.py
+++ Project1/src/PartC.py
@@ -1,24 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from Ridge import Ridge
-from util import FrankeFunction, create_X, ScaleandCenterData
+from Lasso import pureSKLearnLasso
+from util import FrankeFunction, ScaleandCenterData, create_X
 from sklearn.model_selection import train_test_split
 
-lmbdaVals = [0.0001, 0.001, 0.01, 0.1, 1.0]
+alphaVals = [0.0001, 0.001, 0.01, 0.1, 1.0]
 maxDim = 5
 
 
-def RidgeofFranke():
+def LassoofFranke():
     N = 100
     x = np.sort(np.random.uniform(0, 1, N))
     y = np.sort(np.random.uniform(0, 1, N))
     x, y = np.meshgrid(x, y)
     z = FrankeFunction(x, y) + 0.2 * np.random.randn(N, N)
 
-    MSE_trains = [[] for _ in lmbdaVals]
-    MSE_tests = [[] for _ in lmbdaVals]
-    R2s = [[] for _ in lmbdaVals]
+    MSE_trains = [[] for _ in alphaVals]
+    MSE_tests = [[] for _ in alphaVals]
+    R2s = [[] for _ in alphaVals]
 
     for dim in range(maxDim + 1):
         X = create_X(x, y, dim)
@@ -27,8 +27,8 @@ def RidgeofFranke():
         X_train, X_test, y_train, y_test = train_test_split(
             X, ScaleandCenterData(np.ravel(z))
         )
-        for i, lmbda in enumerate(lmbdaVals):
-            scores = Ridge(X_train, X_test, y_train, y_test, lmbda)
+        for i, alpha in enumerate(alphaVals):
+            scores = pureSKLearnLasso(X_train, X_test, y_train, y_test, alpha)
             MSE_trains[i].append(scores[0])
             MSE_tests[i].append(scores[1])
             R2s[i].append(scores[2])
@@ -45,26 +45,26 @@ def plotScores(MSE_train: list, MSE_test: list, R2: list):
     ax1.set_xlabel("# of Polynomial dimensions")
     ax1.set_xticks(xVals)
     ax1.set_ylabel("MSE score", color=color)
-    for i, lmbda in enumerate(lmbdaVals):
-        ax1.plot(xVals, MSE_train[i], label=rf"MSE train $\lambda$={lmbda}")
-        ax1.plot(xVals, MSE_test[i], label=rf"MSE test $\lambda$={lmbda}")
+    for i, alpha in enumerate(alphaVals):
+        ax1.plot(xVals, MSE_train[i], label=rf"MSE train $\alpha$={alpha}")
+        ax1.plot(xVals, MSE_test[i], label=rf"MSE test $\alpha$={alpha}")
     ax1.tick_params(axis="y", labelcolor=color)
 
     ax2 = ax1.twinx()
 
     color = "tab:blue"
     ax2.set_ylabel("R2", color=color)
-    for i, lmbda in enumerate(lmbdaVals):
-        ax2.plot(xVals, R2[i], label=rf"R2 score $\lambda$={lmbda}")
+    for i, alpha in enumerate(alphaVals):
+        ax2.plot(xVals, R2[i], label=rf"R2 score $\alpha$={alpha}")
     ax2.tick_params(axis="y", labelcolor=color)
 
     fig.legend()
-    fig.suptitle("Scores by polynomial degree for Ridge")
+    fig.suptitle("Scores by polynomial degree for Lasso")
     fig.tight_layout()
 
     plt.show()
 
 
 if __name__ == "__main__":
-    scores = RidgeofFranke()
+    scores = LassoofFranke()
     plotScores(*scores)
