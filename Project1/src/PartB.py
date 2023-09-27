Added correct scaling to PartB

Project1/src/PartB.py
@@ -6,15 +6,18 @@ from util import FrankeFunction, create_X, ScaleandCenterData
 from sklearn.model_selection import train_test_split
 
 lmbdaVals = [0.0001, 0.001, 0.01, 0.1, 1.0]
-maxDim = 5
+maxDim = 15
 
 
 def RidgeofFranke():
     N = 100
     x = np.sort(np.random.uniform(0, 1, N))
     y = np.sort(np.random.uniform(0, 1, N))
-    x, y = np.meshgrid(x, y)
-    z = FrankeFunction(x, y) + 0.2 * np.random.randn(N, N)
+    # x, y = np.meshgrid(x, y)
+
+    z_true = FrankeFunction(x, y)
+    # z = z_true + np.random.randn(N * N) * 0.2
+    z = z_true + z_true.mean() * np.random.randn(N) * 0.2
 
     MSE_trains = [[] for _ in lmbdaVals]
     MSE_tests = [[] for _ in lmbdaVals]
@@ -22,13 +25,21 @@ def RidgeofFranke():
 
     for dim in range(maxDim + 1):
         X = create_X(x, y, dim)
-        X = ScaleandCenterData(X)
+        # X = ScaleandCenterData(X)
 
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, ScaleandCenterData(np.ravel(z))
-        )
+        X_train, X_test, y_train, y_test = train_test_split(X, z)
+        X_train_mean = X_train.mean(axis=0)
+        X_train_scaled = X_train - X_train_mean
+        X_test_scaled = X_test - X_train_mean
+
+        y_train_mean = y_train.mean()
+        y_train_scaled = y_train - y_train_mean
+        y_test_scaled = y_test - y_train_mean
+
         for i, lmbda in enumerate(lmbdaVals):
-            scores = Ridge(X_train, X_test, y_train, y_test, lmbda)
+            scores = Ridge(
+                X_train_scaled, X_test_scaled, y_train_scaled, y_test_scaled, lmbda
+            )
             MSE_trains[i].append(scores[0])
             MSE_tests[i].append(scores[1])
             R2s[i].append(scores[2])