e82a59a88e35961a72c0e8f3c564e5cfd955140e
Daniel Steeneveldt <dtsteene@math.uio.no>
2023-10-12 14:31:11 +0200

    fixed resampling

--- Project1/src/misk/resampling.py
+++ Project1/src/misk/resampling.py
@@ -8,12 +8,15 @@ from globals import*
 
 
 def bootstrap_polydegrees(data, polyDegrees, n_boostraps, model = OLS()):
+    """
+    Performs boostrap on different polydegrees
+    """
     n_degrees = len(polyDegrees)
 
     error = np.zeros(n_degrees)
     bias = np.zeros(n_degrees)
     variance = np.zeros(n_degrees)
-    
+
     for j, dim in enumerate(polyDegrees):
         X_train = data.create_X(data.x_train, data.y_train, dim)
         X_test = data.create_X(data.x_test, data.y_test, dim)
@@ -34,6 +37,10 @@ def bootstrap_polydegrees(data, polyDegrees, n_boostraps, model = OLS()):
 
 
 def boostrap_lambdas(data, modelType=Ridge):
+    """
+    performs boostrap on polydegrees and hyperparamater lambds
+    for regularized regression
+    """
     n_degrees = len(polyDegrees)
     n_lambds = lambds.size
 
@@ -64,6 +71,9 @@ def boostrap_lambdas(data, modelType=Ridge):
     return error, bias, variance
 
 def sklearn_cross_val(data, polyDegrees: list[int], nfolds, model = OLS()):
+    """
+    sklearn cross val for on polynomial degrees
+    """
     n_degrees = len(polyDegrees)
 
     error = np.zeros(n_degrees)
@@ -80,6 +90,9 @@ def sklearn_cross_val(data, polyDegrees: list[int], nfolds, model = OLS()):
     return error, variance
 
 def kfold_score_degrees(data, polyDegrees: list[int], kfolds: int, model = OLS()):
+    """
+    HomeCooked cross-val using Kfold. Only for polynomial degrees.
+    """
     n_degrees = len(polyDegrees)
 
     error = np.zeros(n_degrees)
@@ -90,12 +103,12 @@ def kfold_score_degrees(data, polyDegrees: list[int], kfolds: int, model = OLS()
     for i,degree in enumerate(polyDegrees):
         scores = np.zeros(kfolds)
 
-        X = data.create_X(data.x, data.y, degree)
+        X = data.create_X(data.x_, data.y_, degree)
 
         for j, (train_i, test_i) in enumerate(Kfold.split(X)):
 
             X_train = X[train_i]; X_test = X[test_i]
-            z_test = z[test_i]; z_train = z[train_i]
+            z_test = data.z_[test_i]; z_train = data.z_[train_i]
 
 
             model.fit(X_train, z_train)
@@ -108,19 +121,58 @@ def kfold_score_degrees(data, polyDegrees: list[int], kfolds: int, model = OLS()
         variance[i] = scores.std()
     return error, variance
 
-def sklearn_cross_val_lambdas(data, polyDegrees: list[int], nfolds, model = Ridge()):
+def sklearn_cross_val_lambdas(data, polyDegrees: list[int], nfolds, modelType = Ridge):
+    """
+    sklearn cross val for polydegrees and lambda
+    """
     n_degrees = len(polyDegrees)
     n_lambds = len(lambds)
     error = np.zeros((n_degrees, n_lambds))
     variance = np.zeros((n_degrees, n_lambds))
 
     for i,degree in enumerate(polyDegrees):
-        X = data.create_X(x, y, degree)
+        X = data.create_X(data.x_, data.y_, degree)
         for j,lambd in enumerate(lambds):
-
-            scores = cross_val_score(model, X, z,
+            model = modelType(lambd)
+            scores = cross_val_score(model, X, data.z_,
                                      scoring="neg_mean_squared_error", cv=nfolds)
             error[i,j] = -scores.mean()
             variance[i,j] = scores.std()
 
+    return error, variance
+
+
+def kfold_score_degrees(data, polyDegrees: list[int], kfolds: int, modelType = Ridge):
+    """
+    HomeCooked cross-val using Kfold. for polydegrees and lambda.
+    """
+    n_degrees = len(polyDegrees)
+    n_lambds = lambds.size
+
+    error = np.zeros((n_degrees, n_lambds))
+    variance = np.zeros((n_degrees, n_lambds))
+
+    Kfold = KFold(n_splits = kfolds)
+
+    for i,degree in enumerate(polyDegrees):
+        scores = np.zeros(kfolds)
+
+        X = data.create_X(data.x_, data.y_, degree)
+
+        for j, lambd in enumerate(lambds):
+            model = modelType(lambd)
+            for k, (train_i, test_i) in enumerate(Kfold.split(X)):
+
+                X_train = X[train_i]; X_test = X[test_i]
+                z_test = data.z_[test_i]; z_train = data.z_[train_i]
+
+
+                model.fit(X_train, z_train)
+
+                z_pred = model.predict(X_test)
+
+                scores[k] = MSE(z_pred, z_test)
+
+            error[i,j] = scores.mean()
+            variance[i,j] = scores.std()
     return error, variance
