3f4a286b024440a860e6055394112282f5bc6b41
Daniel Steeneveldt <daniel.steeneveldt@gmail.com>
2023-10-16 14:16:29 +0200

    removed depricated files and renamed src_final and daniel_tull(src_final alternative)

--- Project1/Z_deprecated_src/misk/globals.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from pathlib import Path
-
-maxDim = 13
-lambds = np.logspace(-3, 7, 13)
-figsPath = Path(__file__).parent.parent.parent / "figures"
-showPlots = True
-savePlots = False
