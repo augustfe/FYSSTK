940b239eaf4a446869e4dd41f34a0d8bc1703e29
August Femtehjell <august.femtehjell@gmail.com>
2023-11-11 14:26:28 +0100

    Refactored heatmaps

--- Project2/src/grad_analysis.py
+++ Project2/src/grad_analysis.py
@@ -17,9 +17,7 @@ from plotutils import (
 )
 from typing import Callable
 import pandas as pd
-import seaborn as sns
 from sklearn.metrics import mean_squared_error
-import matplotlib.pyplot as plt
 
 
 class GradAnalysis:
@@ -210,39 +208,21 @@ class GradAnalysis:
         heat_rho = np.arctan(np.linspace(0, 10, n_rho))
         heat_rho = heat_rho / np.max(heat_rho)
         heat_eta = np.logspace(-7, -1, n_eta)
-        error_arr = np.zeros((n_rho, n_eta))
-        ynew = self.target_func(self.base_x)
-        for i, rho in enumerate(heat_rho):
-            for j, eta in enumerate(heat_eta):
-                schedule = Momentum(eta, rho)
-                Gradient = Gradients(
-                    self.n_points,
-                    self.x_vals,
-                    self.y_vals,
-                    self.model,
-                    self.method,
-                    schedule,
-                )
-                theta = Gradient.GradientDescent(self.base_theta, self.n_epochs)
-                ypred = Gradient.predict(self.base_x, theta, dim)
-                error_arr[i, j] = mean_squared_error(ynew, ypred)
 
-        fig, ax = plt.subplots()
+        schedulers = [[Momentum(eta, rho) for eta in heat_eta] for rho in heat_rho]
+        error_arr = self.error_per_variables(schedulers, dim)
+
         df = pd.DataFrame(
             error_arr,
             index=[f"{rho:.2f}" for rho in heat_rho],
             columns=[f"{eta:.2e}" for eta in heat_eta],
         )
-        sns.heatmap(df, cmap="viridis", ax=ax)
-        ax.set_xlabel(r"$\eta$")
-        ax.set_ylabel(r"$\rho$")
-        ax.set_title(f"Error after {self.n_epochs} epochs")
-        plt.tight_layout()
-        if savePlots:
-            raise NotImplementedError
-        if showPlots:
-            plt.show()
-        plt.close(fig)
+        plotHeatmap(
+            df,
+            title=f"Error after {self.n_epochs} epochs",
+            x_label=r"$\eta$",
+            y_label=r"$\rho$",
+        )
 
     def adagrad_analysis(
         self,
@@ -339,39 +319,18 @@ class GradAnalysis:
         # Scale rho to be between 0 and 1 (Want to remove the outliers just below 1)
         heat_rho = heat_rho * 2 / 3
         heat_eta = np.logspace(-3, 0, n_eta)
-        error_arr = np.zeros((n_rho, n_eta))
-        ynew = self.target_func(self.base_x)
-        for i, rho in enumerate(heat_rho):
-            for j, eta in enumerate(heat_eta):
-                schedule = AdagradMomentum(eta, rho)
-                Gradient = Gradients(
-                    self.n_points,
-                    self.x_vals,
-                    self.y_vals,
-                    self.model,
-                    self.method,
-                    schedule,
-                )
-                theta = Gradient.GradientDescent(self.base_theta, self.n_epochs)
-                ypred = Gradient.predict(self.base_x, theta, dim)
-                error_arr[i, j] = mean_squared_error(ynew, ypred)
 
-        fig, ax = plt.subplots()
+        schedulers = [
+            [AdagradMomentum(eta, rho) for eta in heat_eta] for rho in heat_rho
+        ]
+        error_arr = self.error_per_variables(schedulers, dim)
+
         df = pd.DataFrame(
             error_arr,
             index=[f"{rho:.2f}" for rho in heat_rho],
             columns=[f"{eta:.2e}" for eta in heat_eta],
         )
-        sns.heatmap(df, cmap="viridis", ax=ax)
-        ax.set_xlabel(r"$\eta$")
-        ax.set_ylabel(r"$\rho$")
-        ax.set_title(f"Error after {self.n_epochs} epochs")
-        plt.tight_layout()
-        if savePlots:
-            raise NotImplementedError
-        if showPlots:
-            plt.show()
-        plt.close(fig)
+        plotHeatmap(df, title=f"Error after {self.n_epochs} epochs")
 
     def adam_analysis(
         self,
@@ -457,40 +416,16 @@ class GradAnalysis:
         heat_rho = np.arctan(np.linspace(0.1, 10, n_rho))
         heat_rho = heat_rho * 2 / 3
         heat_eta = np.logspace(-3, 0, n_eta)
-        error_arr = np.zeros((n_rho, n_eta))
-        ynew = self.target_func(self.base_x)
 
-        for i, rho in enumerate(heat_rho):
-            for j, eta in enumerate(heat_eta):
-                schedule = Adam(eta, rho, 0.999)
-                Gradient = Gradients(
-                    self.n_points,
-                    self.x_vals,
-                    self.y_vals,
-                    self.model,
-                    self.method,
-                    schedule,
-                )
-                theta = Gradient.GradientDescent(self.base_theta, self.n_epochs)
-                ypred = Gradient.predict(self.base_x, theta, dim)
-                error_arr[i, j] = mean_squared_error(ynew, ypred)
+        schedulers = [[Adam(eta, rho, 0.999) for eta in heat_eta] for rho in heat_rho]
+        error_arr = self.error_per_variables(schedulers, dim)
 
-        fig, ax = plt.subplots()
         df = pd.DataFrame(
             error_arr,
             index=[f"{rho:.2f}" for rho in heat_rho],
             columns=[f"{eta:.2e}" for eta in heat_eta],
         )
-        sns.heatmap(df, cmap="viridis", ax=ax)
-        ax.set_xlabel(r"$\eta$")
-        ax.set_ylabel(r"$\rho$")
-        ax.set_title(f"Error after {self.n_epochs} epochs")
-        plt.tight_layout()
-        if savePlots:
-            raise NotImplementedError
-        if showPlots:
-            plt.show()
-        plt.close(fig)
+        plotHeatmap(df, title=f"Error after {self.n_epochs} epochs")
 
     def rms_prop_analysis(
         self,
@@ -557,37 +492,13 @@ class GradAnalysis:
         heat_rho = np.arctan(np.linspace(0.1, 10, n_rho))
         heat_rho = heat_rho * 2 / 3
         heat_eta = np.logspace(-3, -1, n_eta)
-        error_arr = np.zeros((n_rho, n_eta))
-        ynew = self.target_func(self.base_x)
 
-        for i, rho in enumerate(heat_rho):
-            for j, eta in enumerate(heat_eta):
-                schedule = RMS_prop(eta, rho)
-                Gradient = Gradients(
-                    self.n_points,
-                    self.x_vals,
-                    self.y_vals,
-                    self.model,
-                    self.method,
-                    schedule,
-                )
-                theta = Gradient.GradientDescent(self.base_theta, self.n_epochs)
-                ypred = Gradient.predict(self.base_x, theta, dim)
-                error_arr[i, j] = mean_squared_error(ynew, ypred)
+        schedulers = [[RMS_prop(eta, rho) for eta in heat_eta] for rho in heat_rho]
+        error_arr = self.error_per_variables(schedulers, dim)
 
-        fig, ax = plt.subplots()
         df = pd.DataFrame(
             error_arr,
             index=[f"{rho:.2f}" for rho in heat_rho],
             columns=[f"{eta:.2e}" for eta in heat_eta],
         )
-        sns.heatmap(df, cmap="viridis", ax=ax)
-        ax.set_xlabel(r"$\eta$")
-        ax.set_ylabel(r"$\rho$")
-        ax.set_title(f"Error after {self.n_epochs} epochs")
-        plt.tight_layout()
-        if savePlots:
-            raise NotImplementedError
-        if showPlots:
-            plt.show()
-        plt.close(fig)
+        plotHeatmap(df, title=f"Error after {self.n_epochs} epochs")
